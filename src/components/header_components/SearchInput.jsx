4fcbce01a7c59d864af799af3695c821da1566d8
Gustavo Santos <gustavo.st971@gmail.com>
2025-06-16 04:20:52 -0300

    feat: View changer button between active tasks and archived tasks working

--- src/components/header_components/SearchInput.jsx
+++ src/components/header_components/SearchInput.jsx
@@ -11,12 +11,9 @@ const SearchInput = () => {
                 onChange={(e) => setSearchValue(e.target.value)}
                 value={searchValue}
                 type='text'
-                className='h-9 bg-gray-800 text-gray-400 border-1 border-gray-500 rounded rounded-r-none ml-10 px-2'
-                placeholder='Pesquisar Tarefa'
+                className='h-9 bg-gray-800 text-gray-400 border-1 border-gray-500 rounded ml-10 px-2 w-70'
+                placeholder='Pesquisar Nome da Tarefa'
             />
-            <button className='bg-gray-800 border-gray-500 px-3 h-9 rounded rounded-l-none border-y-1 border-r-1 cursor-pointer hover:bg-slate-900 transition-all'>
-                <FaSearch className='text-gray-400' />
-            </button>
         </>
     )
 }
--- src/components/header_components/ViewChanger.jsx
+++ src/components/header_components/ViewChanger.jsx
@@ -1,11 +1,20 @@
 import useViewChangerContext from '../../hook/useViewChangerContext';
 
 const ViewChanger = () => {
-    const { setViewTrigger } = useViewChangerContext();
-    
+    const { viewTrigger, setViewTrigger } = useViewChangerContext();
+
     return (
         <>
-            <button onClick={() => setViewTrigger(prev => !prev)} className='text-gray-400 border-1 border-gray-500 rounded mx-10 p-1 bg-gray-900 hover:bg-gray-700 transition-all cursor-pointer'>Tarefas Arquivadas</button>
+            <button
+                onClick={() => setViewTrigger(false)}
+                className={`${!viewTrigger ? 'bg-gray-500 text-gray-900' : 'bg-gray-900 cursor-pointer text-gray-400 border-1 border-gray-500 hover:bg-gray-700 transition-all'} rounded ml-10 p-1`}>
+                Tarefas Ativas
+            </button>
+            <button
+                onClick={() => setViewTrigger(true)}
+                className={`${viewTrigger ? 'bg-gray-500 text-gray-900' : 'bg-gray-900 cursor-pointer text-gray-400 border-1 border-gray-500 hover:bg-gray-700 transition-all'} rounded ml-10 p-1`}>
+                Tarefas Arquivadas
+            </button>
         </>
     )
 }
